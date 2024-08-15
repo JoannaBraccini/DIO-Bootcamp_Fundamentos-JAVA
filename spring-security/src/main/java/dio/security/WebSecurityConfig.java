d085748903ef40f57b51b42c734e1313266d6d94
Joanna <joannabraccini@gmail.com>
2024-08-15 18:10:56 -0300

    security

--- spring-security/src/main/java/dio/security/WebSecurityConfig.java
+++ spring-security/src/main/java/dio/security/WebSecurityConfig.java
@@ -1,49 +1,37 @@
-package dio.security;
+package dio.spring.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
-        return http.build();
+public class WebSecurityConfig extends WebSecurityConfiguration {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(requests -> requests
+                .requestMatchers("/").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers("/managers").hasAnyRole("MANAGERS")
+                .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated().and().formLogin(withDefaults()));
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("user123")
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}user123")
                 .roles("USERS")
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("master123")
-                .roles("MANAGERS")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+                .and()
+                .withUser("admin")
+                .password("{noop}master123")
+                .roles("MANAGERS");
     }
 }
